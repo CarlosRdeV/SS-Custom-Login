4dbca38502f7e5478fc29844b23f9664f25b56e7
Carlos Rivera <carlosrdev@gmail.com>
2020-02-22 15:30:39 -0600

    working
    solo con usuarios creados de forma manual

--- demologin/src/main/java/com/security/demologin/DemologinApplication.java
+++ demologin/src/main/java/com/security/demologin/DemologinApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemologinApplication {
 
+	// https://www.javainuse.com/spring/boot-jwt
+	// https://www.javainuse.com/spring/boot-jwt-mysql
 	public static void main(String[] args) {
 		SpringApplication.run(DemologinApplication.class, args);
 	}
---
+++ demologin/src/main/java/com/security/demologin/rest/JwtAuthenticationController.java
@@ -0,0 +1,62 @@
+package com.security.demologin.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.demologin.config.JwtTokenUtil;
+import com.security.demologin.model.JwtRequest;
+import com.security.demologin.model.JwtResponse;
+import com.security.demologin.service.JwtUserDetailsService;
+
+@RestController
+@CrossOrigin
+@RequestMapping
+public class JwtAuthenticationController {
+
+	private AuthenticationManager authenticationManager;
+
+	private JwtTokenUtil jwtTokenUtil;
+
+	private JwtUserDetailsService userDetailsService;
+
+	@Autowired
+	public JwtAuthenticationController(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil,
+			JwtUserDetailsService userDetailsService) {
+		this.authenticationManager = authenticationManager;
+		this.jwtTokenUtil = jwtTokenUtil;
+		this.userDetailsService = userDetailsService;
+	}
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+
+}
